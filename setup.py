46dd7b42ef88154c4c1fa74581686c64838223da
Unknown <bkehoe@irobot.com>
2018-02-24 09:28:55 -0500

    Versioning

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup
 
+def find_version(name):
+    import os.path, codecs, re
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, name), 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name='cfn-yaml-tags',
-    version='0.1.0',
+    version=find_version('cfn_yaml_tags.py'),
     description='Support parsing of CloudFormation YAML tags',
     py_modules=["cfn_yaml_tags"],
     install_requires=["pyyaml"],
     author='Ben Kehoe',
     author_email='bkehoe@irobot.com',
     project_urls={
-        "https://github.com/iRobotCorporation/cfn-yaml-tags",
+        "Source code": "https://github.com/iRobotCorporation/cfn-yaml-tags",
     },
     license='Apache Software License 2.0',
     classifiers=(
